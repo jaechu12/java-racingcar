0d7140f094b3695f194ec34be36a5b245b73e7a8
jaechu12 <kys20067765@gmail.com>
2025-03-24 19:52:10 +0900

    입력받은 문장 쪼개기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -16,7 +16,11 @@ public class Application {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String userInput = readLine().trim();
         List<String> carNames = new ArrayList<>();
-        
+
+        for (String name : userInput.split(",")) {
+            carNames.add(name.trim());
+        }
+
     }
 }
 
