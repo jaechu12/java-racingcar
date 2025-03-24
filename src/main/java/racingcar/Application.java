ab9e131104172af0ab82579c92e1ff624e3d7348
jaechu12 <kys20067765@gmail.com>
2025-03-24 18:40:48 +0900

    입력,구분

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String userInput = readLine().trim();
+        List<String> carNames = new ArrayList<>();
+
+        for (String name : userInput.split(",")) {
+            carNames.add(name.trim());
+        }
     }
 }
