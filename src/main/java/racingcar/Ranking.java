f56c8297b15528bd4e8797af1553ae5c16f38234
jaechu12 <kys20067765@gmail.com>
2025-03-31 21:00:15 +0900

    feat: 최고숫자찾기

--- src/main/java/racingcar/Ranking.java
+++ src/main/java/racingcar/Ranking.java
@@ -1,4 +1,21 @@
 package racingcar;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ranking {
+public class Ranking extends Application{
+    public void rank(){
+        int rank2 = 0;
+        for (int k = 0; super.score.size() - 1 > k; k++) {
+            rank2 = Math.max(super.score.get(k),super.score.get(k + 1));
+            if (rank2>super.rank1){
+                super.rank1=rank2;
+            }
+        }
+
+        if (super.carNames.size() == 1) {
+            super.rank1 = super.score.get(0);
+        }
+    }
+
+    
 }
