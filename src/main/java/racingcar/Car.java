03ea37384399b12bca0d82b8a607bc5ca051cf04
jaechu12 <kys20067765@gmail.com>
2025-03-31 20:58:51 +0900

    feat: 랜덤숫자를이용해자동차전진혹은정지출력

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,26 @@
 package racingcar;
 
-public class Car {
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car extends Application{
+
+    public void race(){
+        int CountRound = 0;
+
+        for (int i = 0; i < super.count * super.carNames.size(); i++) {
+
+            if (CountRound == super.carNames.size()) {
+                CountRound = 0;
+            }
+
+
+            int randomInt = Randoms.pickNumberInRange(0, 9);
+            if (randomInt > 3) {
+                super.score.set(CountRound, super.score.get(CountRound) + 1);
+            }
+            CountRound = CountRound + 1;
+        }
+    }
+
+
 }
